dffd795886cedb6726e0e8672fe9b6bd67059038
Simon Fayer <sf105@ic.ac.uk>
2017-10-12 19:28:26 +0100

    Update version in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 # fetch-ban installer
 setup(name = 'fetch-ban',
-      version = '2.0.0',
+      version = '2.0.1',
       description = 'A tool for downloading ban lists',
       scripts = ['fetch-ban'],
       packages = ['FetchBanLib'])
